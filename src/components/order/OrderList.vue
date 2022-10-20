2984802d812d5b707e6af7816474c7ef6ecb5497
kyle <kyle@addtune.net>
2022-10-20 11:18:44 +0900

    skeleton 설정

--- src/components/order/OrderList.vue
+++ src/components/order/OrderList.vue
@@ -1,38 +1,40 @@
 <template>
-  <table class="basic">
-    <caption>
-      출판사별 주문리스트
-    </caption>
-    <thead>
-      <tr>
-        <th>출판사</th>
-        <th>종수</th>
-        <th>주문일시</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>북스피어</td>
-        <td>5</td>
-        <td>2022.10.04 09:41:23</td>
-      </tr>
-      <tr>
-        <td>북스피어</td>
-        <td>5</td>
-        <td>2022.10.04 09:41:23</td>
-      </tr>
-      <tr>
-        <td>북스피어</td>
-        <td>5</td>
-        <td>2022.10.04 09:41:23</td>
-      </tr>
-    </tbody>
-    <tfoot v-if="list.length === 0">
-      <tr>
-        <td colspan="4">주문 리스트가 없습니다.</td>
-      </tr>
-    </tfoot>
-  </table>
+  <section>
+    <table class="basic">
+      <caption>
+        출판사별 주문리스트
+      </caption>
+      <thead>
+        <tr>
+          <th>출판사</th>
+          <th>종수</th>
+          <th>주문일시</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>북스피어</td>
+          <td>5</td>
+          <td>2022.10.04 09:41:23</td>
+        </tr>
+        <tr>
+          <td>북스피어</td>
+          <td>5</td>
+          <td>2022.10.04 09:41:23</td>
+        </tr>
+        <tr>
+          <td>북스피어</td>
+          <td>5</td>
+          <td>2022.10.04 09:41:23</td>
+        </tr>
+      </tbody>
+      <tfoot v-if="list.length === 0">
+        <tr>
+          <td colspan="4">주문 리스트가 없습니다.</td>
+        </tr>
+      </tfoot>
+    </table>
+  </section>
 </template>
 
 <script>
