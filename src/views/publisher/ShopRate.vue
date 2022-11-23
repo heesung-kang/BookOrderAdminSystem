10391ff7a6a181fec2e9c3fb391489a929fa054b
kyle <kyle@addtune.net>
2022-11-23 10:43:41 +0900

    서적별 공급률 적용

--- src/views/publisher/ShopRate.vue
+++ src/views/publisher/ShopRate.vue
@@ -13,11 +13,11 @@
             <th>서점명</th>
             <th>이메일</th>
             <th>서점 공급률</th>
-            <!--            <th></th>-->
+            <th></th>
           </tr>
         </thead>
-        <tbody>
-          <tr v-for="(item, index) in shops" :key="index">
+        <tbody v-for="(item, index) in shops" :key="index">
+          <tr>
             <td>{{ item.data.shop }}</td>
             <td>{{ item.data.email }}</td>
             <td>
@@ -25,12 +25,22 @@
                 <span v-if="rate.sid === sid"><input type="number" v-model="rate.rate" /></span>
               </span>
             </td>
-            <!--          <td><button class="each">서적별 설정</button></td>-->
+            <td><button class="each" @click="booksRateSet(item.uid)">서적별 설정</button></td>
+          </tr>
+          <tr v-if="booksList.length > 0 && item.uid === booksList[0].id">
+            <td colspan="4">
+              <div v-for="book in booksList" :key="book.data.isbn" class="d-flex book-list">
+                <span>{{ book.data.subject }}</span>
+                <span>{{ book.data.isbn }}</span>
+                <span><input type="number" v-model="book.rate" /></span>
+              </div>
+              <div class="btn-save"><button class="primary" @click="booksRateSave(item.uid)">서적별 공급률 저장</button></div>
+            </td>
           </tr>
         </tbody>
         <tfoot v-if="shops.length === 0">
           <tr>
-            <td colspan="3">서점 리스트가 없습니다.</td>
+            <td colspan="4">서점 리스트가 없습니다.</td>
           </tr>
         </tfoot>
       </table>
@@ -42,9 +52,10 @@
 <script>
 import { mapGetters } from "vuex";
 import { db } from "@/utils/db";
-import { collection, query, getDocs, where, writeBatch, doc } from "firebase/firestore";
+import { collection, query, getDocs, where, writeBatch, doc, updateDoc } from "firebase/firestore";
 import { getCookie } from "@/utils/cookie";
 import TableSkeleton from "@/skeletons/TableSkeleton";
+import debounce from "lodash.debounce";
 export default {
   name: "PublisherList",
   components: { TableSkeleton },
@@ -54,7 +65,7 @@ export default {
       shops: [],
       keyword: "",
       sid: "",
-      sids: [],
+      booksList: [],
       oldRate: [],
     };
   },
@@ -64,14 +75,6 @@ export default {
   async created() {
     const infos = getCookie("userInfo");
     this.sid = infos.info.sid;
-    this.sids = [];
-    //출판사 보유 서적 id 불러오기
-    this.$store.commit("common/setSkeleton", true);
-    const first = query(collection(db, "booksData"), where("sid", "==", this.sid));
-    const documentSnapshots = await getDocs(first);
-    documentSnapshots.forEach(doc => {
-      this.sids.push(doc.id);
-    });
     await this.load();
   },
   methods: {
@@ -80,7 +83,6 @@ export default {
       //초기화
       this.origin = [];
       this.shops = [];
-      this.loadRate = [];
       try {
         this.$store.commit("common/setSkeleton", true);
         const first = query(collection(db, "shopInfo"));
@@ -117,6 +119,57 @@ export default {
       } catch (e) {
         this.$store.commit("common/setLoading", false);
         console.log(e);
+      }
+    },
+    //서적별 공급률 설정
+    booksRateSet: debounce(async function (id) {
+      this.$store.commit("common/setLoading", true);
+      this.booksList = [];
+      //출판사 보유 서적 불러오기
+      const first = query(collection(db, "booksData"), where("sid", "==", this.sid));
+      const documentSnapshots = await getDocs(first);
+      documentSnapshots.forEach(doc => {
+        this.booksList.push({ id, data: doc.data(), rate: "" });
+      });
+      //서적별 rate 정보 가져오기
+      const rates = [];
+      this.origin.forEach(elm => {
+        if (elm.uid === id) {
+          elm.data.bookRate.forEach(book => {
+            rates.push(book.data);
+          });
+        }
+      });
+      //서점 정보 불러오기 : 서점정보에 bookRate가 있다면 booksList에 매칭
+      this.booksList.forEach(ele => {
+        rates.forEach(book => {
+          if (book.isbn === ele.data.isbn) {
+            ele.rate = book.rate;
+          }
+        });
+      });
+      this.$store.commit("common/setLoading", false);
+    }, 300),
+    //서적별 공급률 저장
+    async booksRateSave(id) {
+      this.origin.forEach(ele => {
+        if (ele.uid === id) {
+          this.oldRate = ele.data.bookRate;
+        }
+      });
+      this.booksList.forEach(ele => {
+        this.oldRate.push({ uid: id, data: { isbn: ele.data.isbn, rate: ele.rate } });
+      });
+      this.$store.commit("common/setLoading", true);
+      try {
+        await updateDoc(doc(db, "shopInfo", id), {
+          bookRate: this.oldRate,
+        });
+        await this.load();
+        this.$store.commit("common/setLoading", false);
+      } catch (e) {
+        console.log(e);
+        this.$store.commit("common/setLoading", false);
       }
     },
     search() {
@@ -157,7 +210,7 @@ table {
         background-color: deepskyblue;
         color: #fff;
         padding: 0 5px;
-        vorder-radius: 3px;
+        border-radius: 3px;
       }
     }
   }
@@ -180,6 +233,54 @@ table {
     border: 1px solid #000;
     background: #fff;
     margin-left: 10px;
+  }
+}
+.book-list {
+  border-bottom: 1px dashed #cccccc;
+  padding: 3px 9%;
+  justify-content: space-between;
+  span {
+    &:nth-child(1) {
+      width: calc(100% - 290px);
+      text-align: left;
+    }
+    &:nth-child(2) {
+      width: 250px;
+    }
+    &:nth-child(3) {
+      width: 40px;
+      input {
+        width: 40px !important;
+      }
+    }
+  }
+}
+.btn-save {
+  margin: 5px 0 50px;
+  display: flex;
+  justify-content: center;
+  button {
+    @include NotoSans(1.4, 500, #fff);
+  }
+}
+@include mobile {
+  .book-list {
+    padding: 3px;
+    span {
+      &:nth-child(1) {
+        width: calc(100% - 150px);
+        text-align: left;
+      }
+      &:nth-child(2) {
+        width: 110px;
+      }
+      &:nth-child(3) {
+        width: 40px;
+        input {
+          width: 40px !important;
+        }
+      }
+    }
   }
 }
 </style>
